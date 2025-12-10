a2f8b8a14ab4e05892c199555de02083b59ed9e7
orangedragon369-crypto <orangedragon369@gmail.com>
2025-12-10 09:59:13 -0700

    Refactor code structure for improved readability and maintainability

--- node/apps/csv-demo/read-csv.js
+++ node/apps/csv-demo/read-csv.js
@@ -1,22 +1,23 @@
 import fs from 'fs';
 import csv from 'csv-parser';
+import { argv } from 'process';
  
-function readStream(){
+function readStream(source ,data, require){
     const results = [];
 
-    fs.createReadStream('data/input.csv')
+    fs.createReadStream(source)
         .pipe(csv())
         .on('data', (row) => results.push(row))
         .on('end', () => {
-            console.log(`CSV parsed: `, skipAdults(results, 20));
-            
+            console.log(`CSV parsed: `, skipAdults(results, data, require));
+            console.log(skipAdults(results, data, require).length);
         });
 }
 
-function skipAdults(list, age) {
-    const kid = [];
-    for (const man of list) if (man.age < age) kid.push(man);
-    return kid;
+function skipAdults(list, info, req) {
+    const passed = [];
+    for (const obj of list) if (obj[info] < req || obj[info] === req) passed.push(obj);
+    return passed;
 }
 
-readStream();
+readStream(argv[2], argv[3], argv[4]);
