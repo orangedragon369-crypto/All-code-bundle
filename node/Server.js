d2db6f710496c42f41bd4d91ab05d35479f7997c
orangedragon369-crypto <orangedragon369@gmail.com>
2025-12-11 09:20:59 -0700

    feat: establish basic chat server and client connection with welcome message

--- node/Server.js
+++ node/Server.js
@@ -1,5 +1,11 @@
 const net = require('net');
 
 const server = net
+    .createServer((client)=>{
+        client.write("welcome to the chat room.")
+    })
+    .listen(5000, () => {
+        console.log("Listening on port 5000")
+    })
 
-const log = fstat.createWriteStream("server.log");
+//const log = fstat.createWriteStream("server.log");
--- node/client.js
+++ node/client.js
@@ -1,3 +1,11 @@
 const net = require('net');
 
-const client = net.createConnection(5000, ()=> )
+const client = net.createConnection(5000, ()=> {
+    console.log("connected");
+});
+
+client.setEncoding("utf-8");
+client.on("data", (data)=> {
+    console.log("asdfasdfa");
+    console.log(data);
+})
