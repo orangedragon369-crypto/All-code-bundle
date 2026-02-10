903026de40ab5b08183b7ae32967db0264088691
orangedragon369-crypto <orangedragon369@gmail.com>
2026-02-10 09:59:19 -0700

    feat: Update card data structure and enhance filtering logic in API

--- node/apiserver/index.js
+++ node/apiserver/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const fs = require('fs');
 const fsPromise = require("fs/promises");
+const path = require("path");
 
 const app = express();
 const port = process.env.port || 3000;
@@ -18,14 +18,18 @@ app.post('/user', (req, res) => {
 
 app.get('/cards', async (req, res) => {
     try {
-        const file = await fsPromise.readFile(__dirname + "/public/cards.json", "utf-8");
-        const cards = JSON.parse(file);
+        const file = await fsPromise.readFile(path.join(__dirname + "/public/cards.json"), "utf-8");
+        const cards = JSON.parse(file).cardkor;
 
-        for (const key in req.query) {
-            filters[key] = req.query[key];
-        }
+        const filters = { ...req.query };
 
-        res.status(200).json(cards.cards);
+        const results = cards.filter(card =>
+            Object.entries(filters).every(([key, value]) =>
+                card[key] == value
+            )
+        );
+
+        res.status(200).json(results);
     } catch (err) {
         console.error(err);
         res.status(500).send("Error reading cards");
