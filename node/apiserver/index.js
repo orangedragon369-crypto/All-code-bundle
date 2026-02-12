ea7971a74135aae6a180890e37a3a4226f8f4d3d
orangedragon369-crypto <orangedragon369@gmail.com>
2026-02-12 08:04:24 -0700

    feat: Enhance card API with new endpoints for sets, types, rarities, count, and random card retrieval; update cards.json structure and add new card types

--- node/apiserver/index.js
+++ node/apiserver/index.js
@@ -36,6 +36,91 @@ app.get('/cards', async (req, res) => {
     }
 });
 
+app.get('/cards/:what', async (req, res) => {
+    try {
+        const file = await fsPromise.readFile(path.join(__dirname + "/public/cards.json"), "utf-8");
+        const cards = JSON.parse(file);
+
+        let cardCount = 0;
+        switch (req.params.what) {
+            case "sets":
+                return res.status(200).json(
+                    cards.cardkorUtils.sets
+                );
+            case "types":
+                return res.status(200).json(
+                    cards.cardkorUtils.types
+                );
+            case "rarities":
+                return res.status(200).json(
+                    cards.cardkorUtils.rarities
+                );
+            case "count":
+                for (const [] of Object.entries(cards.cardkor[0])) cardCount++;
+                return res.status(200).json(cardCount);
+            case "random":
+                const cardBank = cards.cardkor[0];
+                
+                for (const [] of Object.entries(cardBank)) cardCount++;
+                return res.status(200).json(
+                    cardBank[Math.floor(Math.random() * cardCount)]
+                );
+            default:
+                return res.status(400).json({
+                    "error": "Invalid parameter",
+                    "valid Parameters": [
+                        "sets",
+                        "types",
+                        "rarities",
+                        "count",
+                        "random",
+                        "create",
+                        "update/{id}",
+                        "delete/{id}"
+                    ]
+                });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error reading cards");
+    }
+});
+
+app.put(['/cards/update/:id', "/cards/create"], async (req, res) => {
+    try {
+        const file = await fsPromise.readFile(path.join(__dirname + "/public/cards.json"), "utf-8");
+        const cards = JSON.parse(file);
+
+        if (!cards.cardkor[0][id]){
+            return res.status(404).json(
+                {"error": "The card you are looking of does not exist"}
+            );
+        }
+
+        return;
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error reading cards");
+    }
+})
+
+app.delete('/cards/delete/:id', async (req, res) => {
+    try {
+        const file = await fsPromise.readFile(path.join(__dirname + "/public/cards.json"), "utf-8");
+        const cards = JSON.parse(file);
+
+        if (!cards.cardkor[0][id]){
+            return res.status(404).json(
+                {"error": "The card you are looking of does not exist"}
+            );
+        }
+
+        return;
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error reading cards");
+    }
+})
 
 app.get('/docs', (req, res) => {
     res.sendFile(__dirname + '/public/docs.html');
